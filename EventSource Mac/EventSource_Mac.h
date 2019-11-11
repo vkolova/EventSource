2ae3991b43e6459d49df6524a59bb057a2b9a927
Veselina Kolova <vmkolova@gmail.com>
2019-11-11 13:48:07 +0200

    Add MacOS support

---
+++ EventSource Mac/EventSource_Mac.h
@@ -0,0 +1,19 @@
+//
+//  EventSource_Mac.h
+//  EventSource Mac
+//
+//  Created by Veselina Kolova on 11.11.19.
+//  Copyright © 2019 Andres. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EventSource_Mac.
+FOUNDATION_EXPORT double EventSource_MacVersionNumber;
+
+//! Project version string for EventSource_Mac.
+FOUNDATION_EXPORT const unsigned char EventSource_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EventSource_Mac/PublicHeader.h>
+
+
